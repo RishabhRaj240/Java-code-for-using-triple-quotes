54318e7d66758ea0cf7e083a5caf65fb4d1fe4ae
Rishabh Raj <rishabhraj240405@gmail.com>
2024-04-15 18:04:56 +0530

    Add files via upload

---
+++ TripleQuote.java
@@ -0,0 +1,12 @@
+class TripleQuote {
+    public static void main(String[] args) {
+         System.out.println(
+            """
+            Hari Krishna
+            Naresh i Technologies
+            Ameerpet
+            Hyderabad, 500016
+            """
+         );
+    }
+}
